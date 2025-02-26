8fa96bf02a015251589edf17fad735de9488596f
kusl007 <kushal.bansal.1001@gmail.com>
2025-02-26 15:05:45 +0545

    nesting done

--- server/index.js
+++ server/index.js
@@ -1,14 +1,14 @@
-const express=require('express');
-const {ApolloServer}=require('@apollo/server');
-const {expressMiddleware}=require('@apollo/server/express4')
-const bodyParser=require('body-parser');
-const cors=require('cors');
-const {default:axios}=require('axios');
+const express = require("express");
+const { ApolloServer } = require("@apollo/server");
+const { expressMiddleware } = require("@apollo/server/express4");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const { default: axios } = require("axios");
 
 async function startServer() {
-    const app=express();
-    const server=new ApolloServer({
-        typeDefs:`
+  const app = express();
+  const server = new ApolloServer({
+    typeDefs: `
         type User{
         id:ID!
         name:String!
@@ -21,36 +21,43 @@ async function startServer() {
         id:ID!
         title:String!
         completed:Boolean
+        user:User
         }
         type Query{
         getTodos:[Todo]
         getUsers:[User]
+        getUser(id:ID!):User
         
         }`,
-        resolvers:{
-            Query:{
-                getTodos:async()=>
-                  ( await axios.get('https://jsonplaceholder.typicode.com/todos')).data,
-                getUsers:async()=>
-                  ( await axios.get('https://jsonplaceholder.typicode.com/users')).data
+    resolvers: {
+      Todo: {
+        user: async (todo) =>
+          (
+            await axios.get(
+              `https://jsonplaceholder.typicode.com/users/${todo.userId}`
+            )
+          ).data,
+      },
+      Query: {
+        getTodos: async () =>
+          (await axios.get("https://jsonplaceholder.typicode.com/todos")).data,
+        getUsers: async () =>
+          (await axios.get("https://jsonplaceholder.typicode.com/users")).data,
+        getUser: async (parent, { id }) =>
+          (await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`))
+            .data,
 
-                   
-                
-                // getTodos:()=>[{
-                //     id:1,title:"something",completed:false
-                // }]
-            }
-        }
-    });
-    app.use(bodyParser.json());
-    app.use(cors());
-    await server.start();
-    app.use(
-        '/graphql',
-        expressMiddleware(server)
-    );
-    app.listen(8000);
-    console.log('server started on http://localhost:8000/graphql');
-    
+        // getTodos:()=>[{
+        //     id:1,title:"something",completed:false
+        // }]
+      },
+    },
+  });
+  app.use(bodyParser.json());
+  app.use(cors());
+  await server.start();
+  app.use("/graphql", expressMiddleware(server));
+  app.listen(8000);
+  console.log("server started on http://localhost:8000/graphql");
 }
-startServer()
+startServer();
